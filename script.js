3ce22be3c781c26c06b7c1bfc7aa094a1a88cbb5
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-05-05 09:38:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let bold=document.getElementsByTagName('strong')
 function highlight() {
 	for(let i=0;i<bold.length;i++){
-		bold[i].color=green;
+		bold[i].style.color=green;
 	}
 }
 
 
 function return_normal() {
     for(let i=0;i<bold.length;i++){
-		bold[i].color=black;
+		bold[i].style.color=black;
 	}
 
     
